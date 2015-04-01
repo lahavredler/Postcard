e6f89b069ead2cfd1f77dde58693c1fe0628710e
Lahav Redler <admin@Lahavs-MacBook-Pro.local>
2015-04-01 15:41:54 +0300

    name label functionality

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,9 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var anotherLabel: UILabel!
+    
     @IBOutlet weak var enterNameTextBox: UITextField!
     @IBOutlet weak var enterMessageTextBox: UITextField!
+    
     @IBOutlet weak var mailButton: UIButton!
+    
+    // from here is the new assigenment
+    
+    
     
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         //adding comment here
@@ -21,14 +28,26 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextBox.text
         messageLabel.textColor = UIColor.redColor()
         
+        anotherLabel.hidden = false
+        anotherLabel.text = "Hi \(enterNameTextBox.text)"
+        anotherLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextBox.text = ""
+        enterNameTextBox.resignFirstResponder()
+        
+        
         enterMessageTextBox.text = ""
         // func to remove keyboard after [send mail] pressed
         enterMessageTextBox.resignFirstResponder()
+        
         
         // func to update the mail button title
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
         
     }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
